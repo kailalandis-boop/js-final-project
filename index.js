d83e907debefef604f93206af99e7fc130d08633
Kaila Landis <kailalandis@gmail.com>
2026-09-03 18:29:39 -0400

    feat: modal rework

--- index.js
+++ index.js
@@ -1,30 +1,53 @@
 // API: https://vpic.nhtsa.dot.gov/api/vehicles/getmodelsformakeyear/make/honda/modelyear/2015?format=json //
 // API: https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeYear/make/toyota/modelyear/2020?format=json //
 
-const userListEl = document.querySelector(".user-list");
-
-async function main() {
-    const users = await fetch("https://jsonplaceholder.typicode.com/users");
-    const usersData = await users.json();
-    userListEl.innerHTML = usersData.map((user) => userHTML (user)).join("");
-};
-
-main();
-
-function showUserPosts(id) {
-    localStorage.setItem("id", id);
-    window.location.href = `${window.location.origin}/user.html`
+function openMenu() {
+    document.body.classList.add("menu--open");
 }
 
-function userHTML(user) {
-    return `<div class="user-card" onclick="showUserPosts(${user.id})">
-        <div class="user-card__container">
-            <h3>${user.name}</h3>
-            <p><b>Email:</b> ${user.email}</p>
-            <p><b>Phone:</b> ${user.phone}</p>
-            <p><b>Website:</b><a href="https://${user.website}" target="_blank">
-            ${user.website}
-            </a></p>
-        </div>
-    </div>`;
+function closeMenu() {
+    document.body.classList.remove("menu--open");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const userListEl = document.querySelector(".user-list");
+
+// async function main() {
+//     const users = await fetch("https://jsonplaceholder.typicode.com/users");
+//     const usersData = await users.json();
+//     userListEl.innerHTML = usersData.map((user) => userHTML (user)).join("");
+// };
+
+// main();
+
+// function showUserPosts(id) {
+//     localStorage.setItem("id", id);
+//     window.location.href = `${window.location.origin}/user.html`
+// }
+
+// function userHTML(user) {
+//     return `<div class="user-card" onclick="showUserPosts(${user.id})">
+//         <div class="user-card__container">
+//             <h3>${user.name}</h3>
+//             <p><b>Email:</b> ${user.email}</p>
+//             <p><b>Phone:</b> ${user.phone}</p>
+//             <p><b>Website:</b><a href="https://${user.website}" target="_blank">
+//             ${user.website}
+//             </a></p>
+//         </div>
+//     </div>`;
+// }
