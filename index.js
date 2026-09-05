75958238a92e0dc7b32bb59933dcdc19a68933bb
Kaila Landis <kailalandis@gmail.com>
2026-09-05 10:53:50 -0400

    feat: js function between webpages created

--- index.js
+++ index.js
@@ -6,46 +6,14 @@ function closeMenu() {
     document.body.classList.remove("menu--open");
 }
 
+const input = document.querySelector(".input");
+let searchTerm = "";
 
+function setSearchTerm(event) {
+  searchTerm = event.target.value;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const userListEl = document.querySelector(".user-list");
-
-// async function main() {
-//     const users = await fetch("https://jsonplaceholder.typicode.com/users");
-//     const usersData = await users.json();
-//     userListEl.innerHTML = usersData.map((user) => userHTML (user)).join("");
-// };
-
-// main();
-
-// function showUserPosts(id) {
-//     localStorage.setItem("id", id);
-//     window.location.href = `${window.location.origin}/user.html`
-// }
-
-// function userHTML(user) {
-//     return `<div class="user-card" onclick="showUserPosts(${user.id})">
-//         <div class="user-card__container">
-//             <h3>${user.name}</h3>
-//             <p><b>Email:</b> ${user.email}</p>
-//             <p><b>Phone:</b> ${user.phone}</p>
-//             <p><b>Website:</b><a href="https://${user.website}" target="_blank">
-//             ${user.website}
-//             </a></p>
-//         </div>
-//     </div>`;
-// }
+function routeToCarsPage() {
+  if(searchTerm === "") return;
+  window.location.href = `${window.location.origin}/cars.html?search=${searchTerm}`;
+}
